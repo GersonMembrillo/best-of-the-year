0fd7ef0244f030ad105fb01ec7fa256b5f07a4d3
GersonMembrillo <gersonmembrillo@hotmail.com>
2023-10-03 14:21:53 +0200

    B4 new work, added yday work step 3

--- src/main/java/org/java/app/MainController.java
+++ src/main/java/org/java/app/MainController.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class MainController {
@@ -26,6 +27,29 @@ public class MainController {
         songs.add(new Song(3, "Hey Jude"));
         return songs;
     }
+    
+    @GetMapping("/movies/{id}")
+	public String getMovieID(@PathVariable int id, Model model) {
+		
+		Movie movie = getBestMovies().stream()
+							.filter(m -> m.getId() == id)
+						.findFirst().get();
+		String movieTitle = movie.getTitle();
+		
+		model.addAttribute("movieTitle", movieTitle);
+		
+		return "movie";
+	}
+	@GetMapping("/songs/{id}")
+	public String getSongID(@PathVariable int id, Model model) {
+		
+		Song song = getBestSongs().get(id);
+		String songTitle = song.getTitle();
+		
+		model.addAttribute("songTitle", songTitle);
+		
+		return "song";
+	}
 
     @GetMapping("/")
     public String test(Model model) {
